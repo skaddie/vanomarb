933c34fd6ef76dea36d0cef677a2c28b103cd87a
skaddie <87253680+skaddie@users.noreply.github.com>
2023-04-12 16:33:30 +0800

    Update function.js

--- function.js
+++ function.js
@@ -1,12 +1,15 @@
 $(document).ready(function () {
 
-	const message = `Hi there! It's great to meet you. My name is Van Omar Benaires, and I am a recent graduate who is enthusiastic about exploring the world of technology. At the age of 22, I am excited about what the future holds for me.
+	const message = `
+    Hi there! It's great to meet you. My name is Van Omar Benaires, and I am a recent graduate who is enthusiastic about exploring the world of technology. At the age of 22, I am excited about what the future holds for me.
 
-	One of my biggest passions in life is traveling. I love to explore new places, immerse myself in different cultures, and meet people from all walks of life. In addition to traveling, I also enjoy modifying codes and playing games. These hobbies allow me to sharpen my problem-solving skills and keep my mind sharp.
-	
-	Regarding my professional skills, I have a solid foundation in programming languages such as HTML, CSS, JavaScript, PHP, MySQL, and C#. I am also familiar with various JQuery frameworks and libraries, which allow me to develop creative and dynamic web applications. I am continuously striving to improve my knowledge and skills in the field of programming, and I believe that lifelong learning is critical to success.
-	
-	Thank you for visiting my personal site, and I hope you enjoyed learning a bit more about me. Please feel free to reach out to me if you have any questions or just want to say hello. Cheers!`;
+    One of my biggest passions in life is traveling. I love to explore new places, immerse myself in different cultures, and meet people from all walks of life. In addition to traveling, I also enjoy modifying codes and playing games. These hobbies allow me to sharpen my problem-solving skills and keep my mind sharp.
+
+    Regarding my professional skills, I have a solid foundation in programming languages such as HTML, CSS, JavaScript, PHP, MySQL, and C#. I am also familiar with various JQuery frameworks and libraries, which allow me to develop creative and dynamic web applications. I am continuously striving to improve my knowledge and skills in the field of programming, and I believe that lifelong learning is critical to success.
+
+    Thank you for visiting my personal site, and I hope you enjoyed learning a bit more about me. Please feel free to reach out to me if you have any questions or just want to say hello. Cheers!
+`;
+
 	const typingSpeed = 20;
 
 	window.onload = function () {
